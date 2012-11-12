661088515f18576578179339c8e23819be01e00e
JDStraughan <jdstraughan@gmail.com>
2012-11-12 17:28:32 -0600

    Initial rails 3.2 generated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SlcTagger::Application.config.secret_token = '207e3a9b8154524527774ea32cba846949d10aa9afe7569dee87d689a459cfaf23a62be06c1add0603c8a48c32101980bada18485d4e17832145f51c3cc06ee6'
