3374933aebc525417712a4e8ab8b6bfacdb23a1c
Jason Ellis <jason.ellis@yahoo.com>
2012-11-12 19:13:40 -0600

    LRMI

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,9 +4,6 @@ LRMI::Application.configure do
   # Code is not reloaded between requests
   config.cache_classes = true
 
-  # Google Analytics
-  GA.tracker = "UA-34218293-1"
-
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
