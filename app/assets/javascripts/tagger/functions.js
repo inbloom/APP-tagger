7b4e5289bcb0d3a7205b9cbd4c2007864339a067
Jason Ellis <jason.ellis@yahoo.com>
2012-11-18 11:34:29 -0600

    Rehash education tab update code

--- app/assets/javascripts/tagger/functions.js
+++ app/assets/javascripts/tagger/functions.js
@@ -399,31 +399,31 @@ function setupDisplayFieldsEducationTab(TempObject, selectBox){
 
 
 function educationalTabUpdateDataModel(nameBox, nameInput){
-    for (i = 0; i < boxes; i++) {
-        if (document.checkBoxForm.tagItem[i].checked) {
-            var found = false;
-            var LRMIForm = document.forms.LRMIData;
-            var metaSourceValue = "";
-            var x = 0;
-            for (x=0;x<LRMIForm[nameBox].length;x++)
-            {
-                if (LRMIForm[nameBox][x].selected){
-                    if (found) {
-                        metaSourceValue = metaSourceValue + "," + LRMIForm[nameBox][x].value;
-                    }
-                    else {
-                        metaSourceValue = LRMIForm[nameBox][x].value;
-                        found = true;
-                    }
+
+    $("#multiItemSelector input[type=checkbox]:checked").each(function(i,obj) {
+        var found = false;
+        var LRMIForm = document.forms.LRMIData;
+        var metaSourceValue = "";
+        var x = 0;
+        for (x=0;x<LRMIForm[nameBox].length;x++) {
+            if (LRMIForm[nameBox][x].selected) {
+                if (found) {
+                    metaSourceValue = metaSourceValue + "," + LRMIForm[nameBox][x].value;
+                }
+                else {
+                    metaSourceValue = LRMIForm[nameBox][x].value;
+                    found = true;
                 }
             }
-            if (LRMIForm[nameInput].value != ''){
-                metaSourceValue = metaSourceValue + "," + LRMIForm[nameInput].value;
-            }
-            items[i][nameBox] = metaSourceValue;
         }
-    }
+        if (LRMIForm[nameInput].value != ''){
+            metaSourceValue = metaSourceValue + "," + LRMIForm[nameInput].value;
+        }
+        items[obj.id][nameBox] = metaSourceValue;
+    });
+
     updateTextArea();
+
 }
 
 function blankCurrentAlignment() {
