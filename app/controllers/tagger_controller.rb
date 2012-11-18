b2936c0f41524cdccca793508cac180e089f4f1f
JDStraughan <jdstraughan@gmail.com>
2012-11-18 10:50:45 -0600

    Add save local route for saving to users' machine

--- app/controllers/tagger_controller.rb
+++ app/controllers/tagger_controller.rb
@@ -1,4 +1,10 @@
 class TaggerController < ApplicationController
   def index
   end
+  
+  # Saves a string to a file named filename on a users machine
+  def save_local
+    send_data("#{params[:contents]}", :filename => "#{params[:filename]}", :type => "text/plain")
+  end
+  
 end
