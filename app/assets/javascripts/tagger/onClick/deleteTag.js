40006e49b8fc009738c182f2cbce4d4e42948e77
Jason Ellis <jason.ellis@yahoo.com>
2012-11-20 10:23:00 -0600

    Renaming erbs to html.erbs and movig modals into their own folder

--- app/assets/javascripts/tagger/onClick/deleteTag.js
+++ app/assets/javascripts/tagger/onClick/deleteTag.js
@@ -1,8 +1,11 @@
 $(function() {
 
     $(".delete-tag").live('click', function(obj) {
-        var tag_id = $(obj.target).parent().attr('href');
 
+        var tag_id = $(obj.target).parent().attr('href').substr(1);
+
+console.log(tag_id);
+        return false;
     })
 
 });
