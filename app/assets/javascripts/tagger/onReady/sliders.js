19460567a202ae11753e0666f5537f13ecb2c4cf
Jason Ellis <jason.ellis@yahoo.com>
2012-11-20 16:46:27 -0600

    Refactored slider update and slide code.. now with less suck!

--- app/assets/javascripts/tagger/onReady/sliders.js
+++ app/assets/javascripts/tagger/onReady/sliders.js
@@ -7,16 +7,12 @@ $(function() {
         max: 24,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountyears" ).html("Year");
-            }
-            else if (ui.value == 1){
-                $( "#amountyears" ).html( ui.value + " year " );
-            }
-            else {
-                $( "#amountyears" ).html( ui.value + " years " );
-            }
-            $( "#amountyears" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Year");
+            else if (ui.value == 1) p.html( ui.value + " year " );
+            else p.html( ui.value + " years " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -28,16 +24,12 @@ $(function() {
         max: 11,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountmonths" ).html("Month");
-            }
-            else if (ui.value == 1){
-                $( "#amountmonths" ).html( ui.value + " month " );
-            }
-            else {
-                $( "#amountmonths" ).html( ui.value + " months " );
-            }
-            $( "#amountmonths" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Month");
+            else if (ui.value == 1) p.html( ui.value + " month " );
+            else p.html( ui.value + " months " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -49,16 +41,12 @@ $(function() {
         max: 51,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountweeks" ).html("Week");
-            }
-            else if (ui.value == 1){
-                $( "#amountweeks" ).html( ui.value + " week " );
-            }
-            else {
-                $( "#amountweeks" ).html( ui.value + " weeks " );
-            }
-            $( "#amountweeks" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Week");
+            else if (ui.value == 1) p.html( ui.value + " week " );
+            else p.html( ui.value + " weeks " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -70,16 +58,12 @@ $(function() {
         max: 30,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountdays" ).html("Day");
-            }
-            else if (ui.value == 1){
-                $( "#amountdays" ).html( ui.value + " day " );
-            }
-            else {
-                $( "#amountdays" ).html( ui.value + " days " );
-            }
-            $( "#amountdays" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Day");
+            else if (ui.value == 1) p.html( ui.value + " day " );
+            else p.html( ui.value + " days " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -91,16 +75,12 @@ $(function() {
         max: 23,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amounthours" ).html("Hour");
-            }
-            else if (ui.value == 1){
-                $( "#amounthours" ).html( ui.value + " hour " );
-            }
-            else {
-                $( "#amounthours" ).html( ui.value + " hours " );
-            }
-            $( "#amounthours" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Hour");
+            else if (ui.value == 1) p.html( ui.value + " hour " );
+            else p.html( ui.value + " hours " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -112,16 +92,12 @@ $(function() {
         max: 59,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountminutes" ).html("Minute");
-            }
-            else if (ui.value == 1){
-                $( "#amountminutes" ).html( ui.value + " minute " );
-            }
-            else {
-                $( "#amountminutes" ).html( ui.value + " minutes " );
-            }
-            $( "#amountminutes" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Minute");
+            else if (ui.value == 1) p.html( ui.value + " minute " );
+            else p.html( ui.value + " minutes " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
@@ -133,16 +109,12 @@ $(function() {
         max:59,
         value: 0,
         slide: function( event, ui ) {
-            if (ui.value == 0){
-                $( "#amountseconds" ).html("Second");
-            }
-            else if (ui.value == 1){
-                $( "#amountseconds" ).html( ui.value + " second " );
-            }
-            else {
-                $( "#amountseconds" ).html( ui.value + " seconds " );
-            }
-            $( "#amountseconds" ).val( ui.value );
+            var p = $(ui.handle).parent().prev();
+            if (ui.value == 0) p.html("Second");
+            else if (ui.value == 1) p.html( ui.value + " second " );
+            else p.html( ui.value + " seconds " );
+        },
+        change: function () {
             updateTimeRequired();
         }
     });
