1ceb8bd80e396e38c0c8e7b0b02ba6fb38a638b7
Jason Ellis <jason.ellis@yahoo.com>
2012-11-21 13:35:09 -0600

    Update the title using elipse etc...

--- app/assets/javascripts/tagger/onChange/title.js
+++ app/assets/javascripts/tagger/onChange/title.js
@@ -7,7 +7,9 @@ $(function() {
 
         $("#multiItemSelector input[type=checkbox]:checked").each(function(i,obj) {
             items[obj.id][objName] = metaSourceValue;
-            $("#"+obj.id+"Label span")[0].innerHTML = metaSourceValue;
+
+            var title = (metaSourceValue.length > 25) ? metaSourceValue.substr(0,25) + '&hellip;' : metaSourceValue;
+            $("#"+obj.id+"Label span")[0].innerHTML = title;
         });
 
         updateTextArea();
