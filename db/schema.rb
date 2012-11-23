4a2b30b41e6973a0c8fe28687cc8a4839b7b3127
JDStraughan <jdstraughan@gmail.com>
2012-11-23 17:57:29 -0600

    Updating session_id type

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123210325) do
+ActiveRecord::Schema.define(:version => 20121123230607) do
 
   create_table "tags", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "session_id"
     t.text     "data"
+    t.string   "session_id"
   end
 
 end
