54ada58280f14b86d1481f1b3dfd5f95148ef5e2
Jason Ellis <jason.ellis@yahoo.com>
2012-11-23 17:18:26 -0600

    remove code not needed

--- app/assets/javascripts/tagger/onClick/alignmentCheckbox.js
+++ app/assets/javascripts/tagger/onClick/alignmentCheckbox.js
@@ -2,23 +2,15 @@ $(function() {
 
     $(".alignment-checkbox").live('click', function(evt) {
 
-        cb = evt.target
-        guid = $(cb).attr('value');
-
-        // Dig out the data
-        var formEducationAlignment = alignments[guid].educationalAlignment;
-        var formAlignmentType = alignments[guid].alignmentType;
-        var formDotNotation = alignments[guid].dotNotation;
-        var formDescription = alignments[guid].description;
-        var formItemUrl = alignments[guid].itemURL;
-        var formGuid = alignments[guid].guid;
+        checkbox = evt.target
+        objectHash = $(checkbox).attr('value');
 
         $("#multiItemSelector input[type=checkbox]:checked").each(function(i,obj) {
 
-            if ($(cb).is(':checked')) {
-                items[obj.id].educationalAlignments[guid] = alignments[guid];
+            if ($(checkbox).is(':checked')) {
+                items[obj.id].educationalAlignments[objectHash] = alignments[objectHash];
             } else {
-                delete items[obj.id].educationalAlignments[guid];
+                delete items[obj.id].educationalAlignments[objectHash];
             }
 
         });
