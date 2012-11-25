8f06bbb107fc589e257461cdc0d0d3e7e2ca5b56
Jason Ellis <jason.ellis@yahoo.com>
2012-11-24 21:28:42 -0600

    filename fix

--- app/assets/javascripts/tagger/functions/saveAndExport.js
+++ app/assets/javascripts/tagger/functions/saveAndExport.js
@@ -3,7 +3,7 @@ function saveLocal(str, fileType){
     var date = new Date();
     var form_action = location.protocol + '//' + location.host + "/tagger/save_local/";
     $('<form></form>', { action: form_action, method: 'POST'}).append(
-      $('<input></input>', { name: 'filename', type: 'hidden', value: firstName + lastName + "_" + ISODateString(date)+ fileType })
+      $('<input></input>', { name: 'filename', type: 'hidden', value: fileType + "_" + ISODateString(date)+ fileType })
     ).append(
 			$('<input></input>', { name: 'data', type: 'hidden', value: str })
     ).submit();
