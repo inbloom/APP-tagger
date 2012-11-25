ed61535baf77c37946e5f7200c769f0e906dc77b
JDStraughan <jdstraughan@gmail.com>
2012-11-24 20:25:26 -0600

    Set session guid and save to db.  Just add lri and stir.

--- app/controllers/tagger_controller.rb
+++ app/controllers/tagger_controller.rb
@@ -2,9 +2,23 @@ class TaggerController < ApplicationController
   def index
   end
   
-  # Saves a string to a file named filename on a users machine
+  # Saves a string to a file named filename on a user's machine
   def save_local
-    send_data("#{params[:contents]}", :filename => "#{params[:filename]}", :type => "text/plain")
+    Tag.new(:session_id => request.session_options[:id], :data => params[:data]).save
+    send_data("#{params[:data]}", :filename => "#{params[:filename]}", :type => "text/plain")
+  end
+  
+  # Saves a json formatted string to the db, then formats and sends to LRI
+  def save_lri
+    
+    # TODO - Refactor this when we have sso
+    session[:guid] ||= SecureRandom.uuid
+    
+    # Save tag to db
+    Tag.new(:session_id => session[:guid], :data => params[:data]).save
+    
+    # TODO - send data to LRI
+    
   end
   
 end
