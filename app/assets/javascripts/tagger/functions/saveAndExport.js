cb84eff3fe40d0daa44439cec2fd064627470e06
Jason Ellis <jason.ellis@yahoo.com>
2012-11-26 17:53:17 -0600

    Load JS

--- app/assets/javascripts/tagger/functions/saveAndExport.js
+++ app/assets/javascripts/tagger/functions/saveAndExport.js
@@ -26,12 +26,69 @@ function saveDraft(str){
             items = xhr
         },
         error : function(xhr, txtStatus, errThrown) {
+            // # TODO No. Add a real error modal
             console.log(arguments);
         }
     })
-
 }
 
 function saveRemote(str) {
+// # TODO
+}
 
+// This should ONLY ever be the first thing done after the UI loads.
+// Do it at anyother time and unexpected results could ensue.
+function loadDrafts() {
+    $.ajax({
+        type : "POST",
+        dataType : 'json',
+        url  : "/tagger/load_drafts",
+        // On success update our items list to be what the server sends back
+        // Really nothing should change other than now the items have a UUID
+        success : function(xhr) {
+            items = xhr
+
+            // Uncheck everything when you add a new tag
+            jQuery("#multiItemSelector input[type=checkbox]").each(function(i,obj) {
+                obj.checked = false;
+            });
+            // Update the form
+            updateInputFields();
+
+            if (itemCounter == 0){
+                jQuery("#multiItemSelector").empty();
+            }
+
+            for (itemCounter in items) {
+
+                jQuery("#multiItemSelector").append($("<a href='#"+itemCounter+"' class='pull-right delete-tag'><i class='icon-remove'></i></a>  <a href='#"+itemCounter+"' id='"+itemCounter+"URL' "+ (items[itemCounter]['url']?"":"style='display:none;'") +" class='pull-right render-tag'><i class='icon-share'></i>&nbsp;</a>  <label id='"+itemCounter+"Label' class='checkbox'><input id='"+itemCounter+"' type='checkbox' name='tagItem'/><span>"+items[itemCounter]['title']+"</span></label>"));
+
+                for (objHash in items[itemCounter].educationalAlignments) {
+                    // Create our object
+                    var object = {
+                        'educationalAlignment' : items[itemCounter].educationalAlignments[objHash]['educationAlignment'],
+                        'alignmentType' : items[itemCounter].educationalAlignments[objHash]['alignmentType'],
+                        'dotNotation' : items[itemCounter].educationalAlignments[objHash]['dotNotation'],
+                        'description' : items[itemCounter].educationalAlignments[objHash]['description'],
+                        'itemURL' : items[itemCounter].educationalAlignments[objHash]['itemUrl']
+                    };
+
+                    // Okay add it.
+                    if (alignments[objHash] == undefined) {
+                        $('.noAlignmentsYet').hide();
+                        alignments[objHash] = object;
+                        $('#currentAlignmentTable > tbody:last').append('<tr><td><label class="checkbox"><input type="checkbox" class="alignment-checkbox" value="'+objHash+'" />'+ object.dotNotation +'</label></td><td>'+ capitalize(object.alignmentType) +'</td></tr>');
+                    }
+                }
+
+            }
+
+            updateTextArea();
+
+        },
+        error : function(xhr, txtStatus, errThrown) {
+            // # TODO No. Add a real error modal
+            console.log(arguments);
+        }
+    })
 }
