ad918f1e05f76adcc1d6b5ca383217937fbc3588
Jason Ellis <jason.ellis@yahoo.com>
2012-11-26 11:37:18 -0600

    Adding tag state

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121123230607) do
+ActiveRecord::Schema.define(:version => 20121126173509) do
 
   create_table "tags", :force => true do |t|
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.text     "data"
     t.string   "session_id"
+    t.boolean  "published",  :default => false
   end
 
 end
