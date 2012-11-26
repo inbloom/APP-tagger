dd71e53e75c1017aac609ad2ba2340ed0b6b2bf3
Jason Ellis <jason.ellis@yahoo.com>
2012-11-26 14:17:41 -0600

    Updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 LRMI::Application.routes.draw do
 
   # use a real resource
-  resources :tagger, :only => [ :index, :save_local ]
-  post :save_local, :controller => :tagger, :path => "/tagger/save_local"
-  post :save_lri, :controller => :tagger, :path => "/tagger/save_lri"
-  
+  resources :tagger, :only => [ :index, :save_draft, :save_export, :save_remote ] do
+    collection do
+      post :save_draft, :path => "/save_draft"
+      post :save_export, :path => "/save_export"
+      post :save_remote, :path => "/save_remote"
+    end
+  end
 
   # Until we get the products united, just forward to tagger code
   root :to => 'tagger#index'
