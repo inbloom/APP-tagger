a1189a1fe712db89b13cbaccd740ffa24e3490d7
Jason Ellis <jason.ellis@yahoo.com>
2012-12-05 16:07:01 -0600

    Adding load history route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ LRMI::Application.routes.draw do
   # use a real resource
   resources :tagger, :only => [ :index, :save_draft, :save_export, :save_remote ] do
     collection do
+      post :load_history, :path => "/load_history", :constraints => { :format => /json/ }
       post :load_drafts, :path => "/load_drafts", :constraints => { :format => /json/ }
       post :save_draft, :path => "/save_draft", :constraints => { :format => /json/ }
       post :save_export, :path => "/save_export", :constraints => { :format => /json/ }
