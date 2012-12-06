df6f8f96e5eaa475ba0ec1a7eb18525edc833b60
Jason Ellis <jason.ellis@yahoo.com>
2012-12-05 18:37:49 -0600

    set the href correctly

--- app/assets/javascripts/tagger/onClick/historicItem.js
+++ app/assets/javascripts/tagger/onClick/historicItem.js
@@ -3,7 +3,7 @@ $(function() {
     $("#history a").live('click', function(obj) {
 
         var tag_id = $(this).attr('href').substr(1);
-        $('#resetModal .btn-danger').attr('href', "javascript:resetResource('"+tag_id+"');");
+        $('#resetModal .btn-success').attr('href', "javascript:resetResource('"+tag_id+"');");
         $('#resetModal').modal('show');
 
         return false;
