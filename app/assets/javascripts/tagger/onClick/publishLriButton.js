87753b3dd91ae4d9c6e59e66d7aa9d38d4861884
Jason Ellis <jason.ellis@yahoo.com>
2012-12-11 11:57:28 -0600

    Publish saves draft

--- app/assets/javascripts/tagger/onClick/publishLriButton.js
+++ app/assets/javascripts/tagger/onClick/publishLriButton.js
@@ -1,9 +1,13 @@
 $(function() {
 
     $("#publishLriButton").click( function() {
-        showPleaseWait('Publishing...');
+        showPleaseWait('Publishing...  (This can take some time depending on the number of resources you have selected..)');
 
         if (!$(this).hasClass('disabled')) {
+            // First save the draft state
+            var str = processJSONOutput();
+            saveDraft(str);
+            // Then save those that are checked remotely.
             var str = processJSONOutput(true);
             saveRemote(str, 'LRI');
         }
