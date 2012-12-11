a261942658c1b92f8a80e369c1f95f8320924c21
Jason Ellis <jason.ellis@yahoo.com>
2012-12-11 11:40:54 -0600

    Adding a "please wait" modal

--- app/assets/javascripts/tagger/functions/showMessage.js
+++ app/assets/javascripts/tagger/functions/showMessage.js
@@ -1,7 +1,13 @@
 
 function showMessage(body, header) {
+    $('#pleaseWaitModal').modal('hide');
     if (header == undefined) header = "System Message";
     $('#messageModal .header-text').html(header);
     $('#messageModal .body-text').html(body);
     $('#messageModal').modal('show');
+}
+
+function showPleaseWait(body) {
+    $('#pleaseWaitModal .body-text').html(body);
+    $('#pleaseWaitModal').modal('show');
 }
--- app/assets/javascripts/tagger/onClick/publishLriButton.js
+++ app/assets/javascripts/tagger/onClick/publishLriButton.js
@@ -1,6 +1,8 @@
 $(function() {
 
     $("#publishLriButton").click( function() {
+        showPleaseWait('Publishing...');
+
         if (!$(this).hasClass('disabled')) {
             var str = processJSONOutput(true);
             saveRemote(str, 'LRI');
