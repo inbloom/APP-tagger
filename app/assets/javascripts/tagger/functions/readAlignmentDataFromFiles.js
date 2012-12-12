138b09f2d6faaee2969f407f8c1fdd439a253ff5
Jason Ellis <jason.ellis@yahoo.com>
2012-12-12 10:27:22 -0600

    Fixing Alignment reader and processor to work across all browsers

--- app/assets/javascripts/tagger/functions/readAlignmentDataFromFiles.js
+++ app/assets/javascripts/tagger/functions/readAlignmentDataFromFiles.js
@@ -2,14 +2,18 @@
 function readAlignmentDataFromFiles() {
     $.ajax({
         type: "GET",
-        url: "ccss2asn-math.csv",
+        url: "/ccss2asn-math.csv",
         dataType: "text",
-        success: function(data) {processDataForAlignmentArray(data);}
+        success: function(data) {
+            processDataForAlignmentArray(data);
+        }
     });
     $.ajax({
         type: "GET",
         url: "ccss2asn-ela.csv",
         dataType: "text",
-        success: function(data) {processDataForAlignmentArray(data);}
+        success: function(data) {
+            processDataForAlignmentArray(data);
+        }
     });
 }
