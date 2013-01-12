7e5687bac985667c6b3c83e8264f4c0f9a74decb
Jason Ellis <jason.ellis@yahoo.com>
2013-01-11 21:21:41 -0600

    if they save the tag state, but for some reason the user isn't logged in.. don't throw..

--- app/controllers/tagger_controller.rb
+++ app/controllers/tagger_controller.rb
@@ -121,40 +121,44 @@ class TaggerController < ApplicationController
   # Its possible to need to save the tags from multiple places so lets extract this
   # out into a function of it's own for others to use.. share and share-a-like!
   def save_tags_state tags, publish = false
-    # The object we return to the UI, if any
-    response = {}
-    tags.each do |keyValue|
-      # This is the tag, 0 is the id in the UI which is meaningless
-      tag = keyValue[1]
-      # Remove the ID as it doesn't mean anything and is only used by the UI for internal stuff (if at all)
-      tag.delete('id')
-      # if we have a UUID then we must save to that!
-      unless tag['uuid'].present?
-        # Generate a UUID for this item that will be used when sent to the LRI or any server out in the world.
-        # This is unique to this tag and its history and could come from the client
-        tag['uuid'] ||= SecureRandom.uuid
-      end
-      # This tag has a UUID so we need to load it first
-      found_tag = Tag.find_by_uuid tag['uuid']
-      if found_tag.present?
-        found_tag.user_id = session[:user_id]
-        found_tag.data = tag.to_json
-        found_tag.published = publish
-        found_tag.save()
-      else
-        # Create each tag in the database
-        Tag.create(
-            :user_id => current_user.id,
-            :uuid => tag['uuid'],
-            :data => tag.to_json,
-            :published => publish
-        )
-      end
-      # Rebuild tags object for the UI
-      response[keyValue[0]]= tag
-    end if tags.present?
+    # Dont do this if there is no user
+    if current_user.present?
+      # The object we return to the UI, if any
+      response = {}
+      tags.each do |keyValue|
+        # This is the tag, 0 is the id in the UI which is meaningless
+        tag = keyValue[1]
+        # Remove the ID as it doesn't mean anything and is only used by the UI for internal stuff (if at all)
+        tag.delete('id')
+        # if we have a UUID then we must save to that!
+        unless tag['uuid'].present?
+          # Generate a UUID for this item that will be used when sent to the LRI or any server out in the world.
+          # This is unique to this tag and its history and could come from the client
+          tag['uuid'] ||= SecureRandom.uuid
+        end
+        # This tag has a UUID so we need to load it first
+        found_tag = Tag.find_by_uuid tag['uuid']
+        if found_tag.present?
+          found_tag.user_id = session[:user_id]
+          found_tag.data = tag.to_json
+          found_tag.published = publish
+          found_tag.save()
+        else
+          # Create each tag in the database
+          Tag.create(
+              :user_id => current_user.id,
+              :uuid => tag['uuid'],
+              :data => tag.to_json,
+              :published => publish
+          )
+        end
+        # Rebuild tags object for the UI
+        response[keyValue[0]]= tag
+      end if tags.present?
 
-    response
+      response
+    end
+
   end
 
   # Go through the list of tags the UI expects to be loaded on refresh and leave those.. remove the others.
