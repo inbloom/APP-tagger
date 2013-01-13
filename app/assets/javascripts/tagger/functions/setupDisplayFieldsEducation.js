736a9e4d7650fb13daf514162b5aac37ea8a84a7
Jason Ellis <jason.ellis@yahoo.com>
2013-01-13 15:34:13 -0600

    Adding license header to all javascript (and coffee script) files

--- app/assets/javascripts/tagger/functions/setupDisplayFieldsEducation.js
+++ app/assets/javascripts/tagger/functions/setupDisplayFieldsEducation.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012-2013 inBloom, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 // Take the comma separated list for {field} from {item}.{field} and populate the select#{field} box
 // Any of the values in the comma separated list for {field} that doesn't have an option in select#{field}
 // Get stuffed into {field}Other as it's own comma separated list.
