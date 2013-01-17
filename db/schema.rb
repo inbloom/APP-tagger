c167164026a5008f831cd63c4cef2df227ebf063
JDStraughan <jdstraughan@gmail.com>
2013-01-17 15:11:32 -0600

    new schema file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121128231346) do
+ActiveRecord::Schema.define(:version => 20130117205608) do
+
+  create_table "images", :force => true do |t|
+    t.string   "image"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "tags", :force => true do |t|
     t.datetime "created_at",                    :null => false
