df4456d94b7d34947ed65146167f5ab06e14b797
JDStraughan <jdstraughan@gmail.com>
2013-01-17 21:59:31 -0600

    Just for ey (testing)

--- config/boot.rb
+++ config/boot.rb
@@ -21,4 +21,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-require File.expand_path('./exports', File.dirname(__FILE__)) if File.exists?(File.expand_path('./exports', File.dirname(__FILE__)))
+require File.expand_path('./exports', File.dirname(__FILE__))
