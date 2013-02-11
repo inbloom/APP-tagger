e8542d678dd31fb5e1ff28ca39c210c21858fced
JDStraughan <jdstraughan@gmail.com>
2013-02-11 16:57:14 -0600

    Updating when createdOn is updated, allowing for null dates

--- app/assets/javascripts/tagger/onReady/createdOn.js
+++ app/assets/javascripts/tagger/onReady/createdOn.js
@@ -22,16 +22,13 @@ $(function() {
         format: 'yyyy-mm-dd',
         showButtonPanel: true
     }).on('changeDate', function(obj){
-
-        var metaSourceValue = $(obj.target).val();
-
-        $("#multiItemSelector input[type=checkbox]:checked").each(function(i,obj) {
-            items[obj.id][objName] = metaSourceValue;
-        });
-
-        updateTextArea();
-
         $(this).datepicker('hide').blur();
     });
+    $("#"+objName).blur(function(obj) {
+      $("#multiItemSelector input[type=checkbox]:checked").each(function(i,obj) {
+          items[obj.id][objName] = $("#"+objName).val();
+      });
+      updateTextArea();
+    })
 
 });
