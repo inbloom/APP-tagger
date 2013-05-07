41262349160c86a587ccb14c4324f9dca4b558b0
Jason Ellis <jason.ellis@yahoo.com>
2013-05-07 12:02:11 -0500

    If you select all and show the delete all button, but delete them individually it now correctly still hides the delete all button and unchecks the select all button

--- app/assets/javascripts/tagger/functions/deleteTag.js
+++ app/assets/javascripts/tagger/functions/deleteTag.js
@@ -16,23 +16,49 @@
 
 // Delete tag by ID
 function deleteTag(tag_id) {
+  if (tag_id == 'selected') {
+    $("#multiItemSelector input[type=checkbox]:checked").not('.deleted').each(function() {
+      var id = $(this).prop('id');
+      // Delete the item from the array
+      if (items[id] != undefined) {
+        delete items[id];
+        removeUITagElements(id);
+      }
+    });
+
+    // Uncheck the select all checkbox
+    $("#selectDeselectAllResources").prop('checked', false);
+    // hide delete all tags
+    $("#deleteAllTags").hide();
+
+  } else {
     // Delete the item from the array
     if (items[tag_id] != undefined) {
-        delete items[tag_id];
+      delete items[tag_id];
+      removeUITagElements(tag_id);
     }
-    // Remove the refresh and delete buttons too
-    if ($('#'+tag_id).length > 0) {
-        $('#'+tag_id).addClass('deleted');
-        $('#'+tag_id).parent().hide().addClass('deleted');
-        $('a[href=#'+tag_id+']').remove();
 
-        // Update the number of items
-        $("#resourceCount").html($("#multiItemSelector input[type=checkbox]:checked").not('.deleted').length + " of " + $("#multiItemSelector input[type=checkbox]").not('.deleted').length + " resources");
-
+    if ($("#multiItemSelector input[type=checkbox]:checked").not('.deleted').length == 0) {
+      // Uncheck the select all checkbox
+      $("#selectDeselectAllResources").prop('checked', false);
+      // hide delete all tags
+      $("#deleteAllTags").hide();
     }
-    // Hide the modal
-    $("#deleteModal").modal('hide');
-    // Redraw the output
-    updateTextArea();
-    return false;
+  }
+  // Update the 0 of 0 resources counter
+  updateResourceCount();
+  // Hide the modal
+  $("#deleteModal").modal('hide');
+  // Update the text area
+  updateTextArea();
+  return false;
+}
+
+// Remove the refresh and delete buttons too
+function removeUITagElements(tag_id) {
+  if ($('#'+tag_id).length > 0) {
+    $('form#checkBoxForm').find('#'+tag_id).addClass('deleted');
+    $('form#checkBoxForm').find('#'+tag_id).parent().hide().addClass('deleted');
+    $('form#checkBoxForm').find('a[href=#'+tag_id+']').remove();
+  }
 }
